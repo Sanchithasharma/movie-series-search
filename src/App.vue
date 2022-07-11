848ae3f06c4e294f264db1bdf9a45b7187592efc
Sanchithasharma <sanchitha.s.r@gmail.com>
2022-07-11 14:21:39 +0930

    added sass to the existing project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
+<div class="app">
+  <h2>movie search and series search</h2>
   <MovieSearch />
+</div>
+
 </template>
 
 <script>
@@ -14,5 +18,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  $color: red;
+
+  .app {
+    
+    h2 {
+      color: blue;
+      border: 1px solid red;
+    }
+  }
+
 </style>
