8823afbcdf05255e774ada2774bf296d5560b353
Sanchithasharma <sanchitha.s.r@gmail.com>
2022-07-14 07:45:27 +0930

    added input styling

--- src/views/MovieSearch.vue
+++ src/views/MovieSearch.vue
@@ -1,26 +1,21 @@
 <template>
   <div class="movie-search">
     <el-menu
-      class="el-menu-demo"
+      class="movie-search-menu-bar"
       mode="horizontal"
       background-color="#666666"
     >
-      <el-input v-model="input1" placeholder="Type something">
-        <template #prefix>
-          <el-icon class="el-input__icon"><search /></el-icon>
-        </template>
-      </el-input>
-
-      <div class="slidecontainer">
-        <input type="range" min="1" max="100" value="50" />
+      <div class="search-container">
+        <el-input
+          v-model="searchString"
+          placeholder="Search for movie, series or episode ..."
+        >
+          <template #prefix>
+            <el-icon class="el-input__icon"><search /></el-icon>
+          </template>
+        </el-input>
       </div>
 
-      <el-radio-group @change="chooseTypeOfList" v-model="radio">
-        <el-radio label="" size="small">Any</el-radio>
-        <el-radio label="movie" size="small">Movies</el-radio>
-        <el-radio label="series" size="small">Series</el-radio>
-        <el-radio label="episode" size="small">Episode</el-radio>
-      </el-radio-group>
     </el-menu>
   </div>
 </template>
@@ -28,12 +23,12 @@
 <script>
 import { ref } from "vue";
 
-// const radio3 = ref('1')
 export default {
   data() {
     return {
       radio: ref(""),
-      input1: "",
+      searchString: "",
+      value2: ref(0),
     };
   },
   methods: {
@@ -44,26 +39,35 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .el-menu {
   height: 100px;
 
   .el-radio-group {
-    // margin-left: 50%;
-    span.el-radio__label {
+    label.el-radio.el-radio--small {
       color: white;
     }
   }
-
-  .el-input {
-    height: 40px;
-    margin: 35px;
-    width: 20%;
-  }
-
-  .slidecontainer {
-    display: flex;
-    margin: 2px 40px;
+  .search-container {
+    .el-input {
+      height: 40px;
+      margin: 35px;
+      width: 160%;
+    }
   }
 }
+
+.slidecontainer {
+  display: flex;
+  margin: 2px 40px;
+}
+
+// .el-input.el-input--prefix {
+//   border: none;
+//   border-bottom: 1px solid white;
+//   width: 40%;
+//   .el-input__wrapper {
+//     background-color: black;
+//   }
+// }
 </style>
