2ee0dd004a537f490dd79f01f2919e47ff7c4ed2
Sanchithasharma <sanchitha.s.r@gmail.com>
2022-07-15 16:23:03 +0930

    added search bar that has better interface

--- src/views/MovieSearch.vue
+++ src/views/MovieSearch.vue
@@ -1,21 +1,52 @@
 <template>
   <div class="movie-search">
     <el-menu
-      class="movie-search-menu-bar"
+      class="el-menu"
       mode="horizontal"
+      :ellipsis="false"
       background-color="#666666"
+      text-color="#fff"
+      @select="handleSelect"
     >
-      <div class="search-container">
+      <div class="search-input">
         <el-input
-          v-model="searchString"
-          placeholder="Search for movie, series or episode ..."
+          v-model="input4"
+          class="w-50 m-2"
+          placeholder="Type something"
         >
           <template #prefix>
             <el-icon class="el-input__icon"><search /></el-icon>
           </template>
         </el-input>
+        <el-button>Submit</el-button>
       </div>
-      <el-button @click="searchShowByString">Submit</el-button>
+
+      <div class="flex-grow" />
+      <div class="year-slider">
+        <el-label style="font-size: small">YEAR</el-label>
+        <el-slider
+          style="width: 160px"
+          v-model="value"
+          range
+          show-stops
+          :min="1970"
+          :max="2022"
+          :marks="marks"
+        />
+      </div>
+      <div class="show-type">
+        <el-label style="font-size: small">TYPE</el-label>
+        <el-radio-group
+          @change="chooseTypeOfList"
+          text-color="white"
+          v-model="radio"
+        >
+          <el-radio label="" :size="radioSize">Any</el-radio>
+          <el-radio label="movie" :size="radioSize">Movies</el-radio>
+          <el-radio label="series" :size="radioSize">Series</el-radio>
+          <el-radio label="episode" :size="radioSize">Episode</el-radio>
+        </el-radio-group>
+      </div>
     </el-menu>
   </div>
 </template>
@@ -27,8 +58,13 @@ export default {
   data() {
     return {
       radio: ref(""),
+      radioSize: 'small',
       searchString: "",
-      value2: ref(0),
+      value: ref([1990, 2015]),
+      marks: {
+        1970: "1970",
+        2022: "2022",
+      },
     };
   },
   methods: {
@@ -46,36 +82,39 @@ export default {
 <style scoped lang="scss">
 .el-menu {
   height: 100px;
+  color: white;
+  display: flex;
+  align-items: center;
+  .search-input {
+    display: flex;
+    margin: 2vh 2vh;
+    width: 56vh;
+    .el-button {
+      margin: 1vh 1vh;
+    }
+  }
 
+  .show-type {
+    display: flex;
+    flex-direction: column;
+    margin: 2vh 4vh;
+  }
   .el-radio-group {
+    margin-right: 2vh;
+
     label.el-radio.el-radio--small {
       color: white;
     }
-  }
-  .movie-search-menu-bar {
-    display: flex;
-    flex-direction: row;
-  }
-  .search-container {
-    .el-input {
-      height: 40px;
-      margin: 35px;
-      width: 160%;
+    .el-radio__input.is-checked + .el-radio__label {
+      color: white;
     }
   }
+  .year-slider {
+    margin: 2vh 2vh;
+  }
 }
 
-.slidecontainer {
-  display: flex;
-  margin: 2px 40px;
+.flex-grow {
+  flex-grow: 1;
 }
-
-// .el-input.el-input--prefix {
-//   border: none;
-//   border-bottom: 1px solid white;
-//   width: 40%;
-//   .el-input__wrapper {
-//     background-color: black;
-//   }
-// }
 </style>
